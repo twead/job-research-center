7adacf6be62f599fddba53b778c58a9307d12408
Twead <45719524+twead@users.noreply.github.com>
2021-08-30 17:24:44 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# job-research-center
+ 
---
+++ src/test/java/com/szakdolgozat/SzakdolgozatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szakdolgozat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzakdolgozatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
